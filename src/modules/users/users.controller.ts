49f963532163cbc2ef8312e6f7a961660f490d36
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-09-17 01:04:41 +0600

    feat: users fatching api implemented

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  /**
+   * Get all users with optional pagination.
+   * @param query - Filtering and pagination parameters.
+   */
+  @Get()
+  async findAll(@Query() query: Record<string, string>) {
+    return this.service.findAll(query);
+  }
+
+  /**
+   * Get a user by their ID.
+   * @param id - The ID of the user to retrieve.
+   */
+  @Get('/id')
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    return this.service.findById(id);
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/cores/modules/prisma/prisma.service';
+import { paginate } from 'src/utils/paginate';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * Fetch all users with pagination.
+   * @param query - filtering and pagination parameters.
+   */
+  async findAll(query: Record<string, string>) {
+    const page = parseInt(query.page) || 1;
+    const limit = parseInt(query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const [users, total] = await this.prisma.$transaction([
+      this.prisma.user.findMany({
+        skip,
+        take: limit,
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          avatarId: true,
+          avatar: true,
+        },
+      }),
+      this.prisma.user.count(),
+    ]);
+
+    return paginate(users, { total, page, limit });
+  }
+
+  /**
+   * Find a user by their ID.
+   * @param id - The ID of the user to find.
+   */
+  async findById(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    return user;
+  }
+}
