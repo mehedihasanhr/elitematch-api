da2aa4f9e87b8dbf99ea4d02a5fcbeb36b230709
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-09-21 22:07:24 +0600

    fix: match-makers docs update

--- src/modules/match-makers/dto/create-match-maker.dto.ts
+++ src/modules/match-makers/dto/create-match-maker.dto.ts
@@ -1,12 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsBoolean, IsInt, IsNotEmpty } from 'class-validator';
 
 export class CreateMatchMakerDto {
+  @ApiProperty({
+    example: 8,
+    description: 'User ID associated with the match maker',
+    required: true,
+  })
   @Type(() => Number)
   @IsInt()
   @IsNotEmpty({ message: 'Experience year is required' })
   experienceYear: number;
 
+  @ApiProperty({
+    example: true,
+    description: 'Active status of the match maker',
+    required: false,
+    type: Boolean,
+  })
   @IsBoolean()
   @IsNotEmpty({ message: 'Active status is required' })
   active?: true;
