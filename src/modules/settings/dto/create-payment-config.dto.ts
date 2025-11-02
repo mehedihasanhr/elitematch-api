908d991f1d9ad8d911159d54005eeb11dd601dd9
MD Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-11-02 22:46:24 +0600

    change

---
+++ src/modules/settings/dto/create-payment-config.dto.ts
@@ -0,0 +1,60 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { PaymentProvider } from '@prisma/client';
+import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator';
+import { ToBoolean } from 'src/cores/decorators/to-boolean.dto';
+
+export class CreatePaymentConfigDto {
+  @ApiProperty({
+    example: PaymentProvider.STRIPE,
+    description: 'The payment provider to be used',
+  })
+  @IsEnum(PaymentProvider)
+  provider: PaymentProvider;
+
+  @ApiProperty({
+    example: '4eC39HqLyjWDarjtT1zdp7dc',
+    description: 'The API key for the payment provider',
+  })
+  @IsString()
+  secretApiKey: string;
+
+  @ApiProperty({
+    example: '4eC39HqLyjWDarjtT1zdp7dc',
+    description: 'The Public API key for the payment provider',
+  })
+  @IsString()
+  @IsOptional()
+  publicApiKey: string;
+
+  @ApiProperty({
+    example: 'whsec_xxx',
+    description: 'The webhook secret for the payment provider',
+  })
+  @IsString()
+  webhookSecret: string;
+
+  @ApiProperty({
+    example: 'v1',
+    description: 'Api version if applicable',
+  })
+  @IsString()
+  @IsOptional()
+  version: string;
+
+  @ApiProperty({
+    example: true,
+    description: 'Indicates if this payment configuration is active',
+  })
+  @ToBoolean()
+  @IsBoolean()
+  @IsOptional()
+  isActive: boolean;
+
+  @ApiProperty({
+    example: true,
+    description: 'Indicates if this payment configuration is active',
+  })
+  @IsEnum(['test', 'live'])
+  @IsOptional()
+  mode: string;
+}
