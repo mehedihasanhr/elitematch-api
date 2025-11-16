cb2e0cfb9521e2717dd14583795b7d1f8bdc8ba8
MD. Mehedi Hasan <mehedihasan.hr.324@gmail.com>
2025-11-16 22:52:16 +0600

    chome: remove unuse veriable

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
